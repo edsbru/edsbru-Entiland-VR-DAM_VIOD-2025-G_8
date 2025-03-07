be4addb4031a28a5ec1f6b89b357a42187f311f0
edsbru <116338445+edsbru@users.noreply.github.com>
2025-03-07 13:43:58 +0100

    Dum Sounds added

--- Assets/Projects/2025/DAM_AJEI/G_8/Scripts/TargetLogic.cs
+++ Assets/Projects/2025/DAM_AJEI/G_8/Scripts/TargetLogic.cs
@@ -10,6 +10,8 @@ namespace EntilandVR.DosCinco.DAM_AJEI.G_Ocho
         [SerializeField] private GameObject _brokenTarget;
         [SerializeField] private GameObject _targetChild;
         [SerializeField] private TargetType _targetType;
+        [SerializeField] private AudioSource _normalBreakSound;
+        [SerializeField] private AudioSource _specialBreakSound;
 
         void TargetHit()
         {
@@ -32,11 +34,13 @@ namespace EntilandVR.DosCinco.DAM_AJEI.G_Ocho
         void NormalTargetHit()
         {
             ScoreManagerSingleton.Instance.AddScore(100);
+            _normalBreakSound.Play();
         }
 
         void SpecialTargetHit()
         {
             ScoreManagerSingleton.Instance.AddScore(500);
+            _specialBreakSound.Play();
         }
 
         void ObstacleTargetHit()
